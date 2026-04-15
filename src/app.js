4a362003ed75cb2b001fa440d086205d70bfebee
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-15 09:12:16 +0100

    Add withdrawal routes to the application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import transferRoutes from './routes/transfer.routes.js';
+import withdrawalRoutes from "./routes/withdrawal.routes.js";
+
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/transfer', transferRoutes);
+app.use("/api/withdraw", withdrawalRoutes);
+
 
 app.listen(5000, () => console.log('Server running'));
