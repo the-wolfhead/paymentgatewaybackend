b0e8f9eb86a1eaf0744785710282db9f6f3c513d
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-15 10:16:56 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import transferRoutes from './routes/transfer.routes.js';
 import withdrawalRoutes from "./routes/withdrawal.routes.js";
+import escrowRoutes from "./routes/escrow.routes.js";
+
 
 
 const app = express();
@@ -8,6 +10,8 @@ app.use(express.json());
 
 app.use('/api/transfer', transferRoutes);
 app.use("/api/withdraw", withdrawalRoutes);
+app.use("/api/escrow", escrowRoutes);
+
 
 
 app.listen(5000, () => console.log('Server running'));
