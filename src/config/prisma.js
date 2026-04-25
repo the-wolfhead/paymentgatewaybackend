ed700bb97b43adb4acda4c80b1890705f8d8428f
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-25 19:25:05 +0100

    Update prisma.js

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,2 +1,11 @@
-import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+})
+
+export default prisma
