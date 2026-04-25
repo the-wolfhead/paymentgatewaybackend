40a001d07273636b296eb6f0bb5d8312a0f6065a
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-25 20:03:46 +0100

    installed a prisma adapter

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,10 +1,5 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
